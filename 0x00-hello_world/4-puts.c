9baaca3f5bff3435e98b231b264b8b7df8c56996
jassemb <benalijassem201@gmail.com>
2021-06-17 04:11:47 -0700

    puts func

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - puts fuunction
+ *
+ * Return: Always 0 (Success)
+ */ 
+int main()
+{
+   puts("\"Programming is like building a multilingual puzzle");
+   return (0);
+}
