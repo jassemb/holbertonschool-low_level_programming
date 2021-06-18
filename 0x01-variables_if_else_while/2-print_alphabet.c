52e5d2036d1dfe4402fccbf00c52e8a1879fdad4
jassemb <benalijassem201@gmail.com>
2021-06-18 04:52:34 -0700

    alphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -6,10 +6,11 @@
  */
 int main(void)
 {
- char ch;
-   for(ch = 'a' ; ch <= 'z' ; ch++)
+char ch;
+for(ch = 'a' ; ch <= 'z' ; ch++)
    {
       putchar(ch);
    }
+    putchar('\n');
 return (0);
 }
