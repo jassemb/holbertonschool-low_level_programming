0a0e7e09385b497e3f32f685df6c5ac34c17fbe0
jassemb <benalijassem201@gmail.com>
2021-06-18 05:56:42 -0700

    ,__

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,13 +9,14 @@ int main(void)
 int num;
 for (num = 0; num <= 9; num++)
 {
-	putchar((num % 10) + '0');
+putchar((num % 10) + '0');
+
 if (num == 9)
 {
-	continue;	
-	putchar(',');
-	putchar(' ');
-}
+	continue;
+}		
+putchar(',');
+putchar(' ');
 }
 putchar('\n');
 return (0);
