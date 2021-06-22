4c25112cbc23e9c08aa429b2b8c5b24b78e21400
jassemb <benalijassem201@gmail.com>
2021-06-22 07:01:00 -0700

    holberton putchar

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -9,7 +9,7 @@ int main(void)
         char ch[100]="Holberton";
         int i;
         for (i = 0; i <=9; i++)
-                putchar(ch[i]);
-        putchar('\n');
+                _putchar(ch[i]);
+       _putchar('\n');
 	return (0);
 }
