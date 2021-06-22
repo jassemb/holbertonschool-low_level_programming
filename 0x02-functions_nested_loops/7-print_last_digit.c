1757ba20f16850edeecc3cce666338c922644b59
jassemb <benalijassem201@gmail.com>
2021-06-22 13:55:29 -0700

    print_last_digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,7 +1,7 @@
 #include "holberton.h"
 /**
  * print_last_digit - function that prints the last digit of a number %10
- * @n: The number to find the last digit of 
+ * @n: The number to find the last digit of
  *
  * Return: n
  */
