0032ab9b709bb04e024d97bc6622a31193746cd2
jassemb <benalijassem201@gmail.com>
2021-06-22 07:26:46 -0700

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a' ; ch >= 'z' ; ch++)
+	_putchar(ch);
+_putchar('\n');
+return (0);
+}
