60a85ae0163fe2cbb37d9fbd17bcede3f2705508
jassemb <benalijassem201@gmail.com>
2021-06-23 15:57:14 -0700

    multi

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -14,33 +14,34 @@ void print_times_table(int n)
 		{
 			_putchar('0');
 
-			for (mult = 1; mult <= n; mult++)
-			{
-				 _putchar(',');
-				 _putchar(' ');
+				for (mult = 1; mult <= n; mult++)
+				{
+					 _putchar(',');
+					 _putchar(' ');
 
-				 prod = num * mult;
-				if (prod <= 99)
-				{
-					_putchar(' ');
-				}
+					 prod = num * mult;
+					if (prod <= 99)
+					{
+						_putchar(' ');
+					}
 
-				if (mult <= 9)
-				{
-					_putchar(' ');
-				}
-				if (prod <= 100)
-				{
-					_putchar((prod / 100) + '0');
-					_putchar((prod / 100) % 10 + '0');
-				}
-				else if (prod <= 99 && prod >= 10)
-				{
-					_putchar((prod / 10) + '0');
+					if (mult <= 9)
+					{
+						_putchar(' ');
+					}
+					if (prod <= 100)
+					{
+						_putchar((prod / 100) + '0');
+						_putchar((prod / 100) % 10 + '0');
+					}
+					else if (prod <= 99 && prod >= 10)
+					{
+						_putchar((prod / 10) + '0');
+					}
 				}
 			}
 			_putchar((prod % 10) + '0');
 		}
 		 _putchar('\n');
-	}
 }
+
