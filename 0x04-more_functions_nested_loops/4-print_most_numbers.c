8dc0d9cab29d958704b27690f0e3b9c32bf442b5
jassemb <benalijassem201@gmail.com>
2021-06-24 04:26:23 -0700

    print most numbers

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,6 +1,6 @@
 #include "holberton.h"
 /**
- * print_numbers -  prints the numbers, from 0 to 9 except 2 and 4
+ * print_most_numbers -  prints the numbers, from 0 to 9 except 2 and 4
  *
  * Return: 0
  */
