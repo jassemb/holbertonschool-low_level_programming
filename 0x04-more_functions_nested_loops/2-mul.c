05448c21e5cd3844934d0f0b9785434afae8a1dd
jassemb <benalijassem201@gmail.com>
2021-06-24 03:38:12 -0700

    multiplaction

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+ * mul - function that multiplies two integers
+ *  @b: number for multipliction
+ *  @a: number for multipliction
+ *
+ *
+ * Return: 0
+ */
+int mul(int a, int b)
+{
+	n = a * b;
+	_putchar(n)
+	return (0);
+}
