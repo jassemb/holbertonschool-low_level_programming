17b7c26347a6dfd28048d39f16892001b91bb9d0
jassemb <benalijassem201@gmail.com>
2021-06-24 04:23:10 -0700

    print number from 0 to 9

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,17 +1,14 @@
 #include "holberton.h"
+
 /**
- * print_numbers -  prints the numbers, from 0 to 9
- *
- *
+ * print_numbers - Prints the numbers from 0-9.
  */
 void print_numbers(void)
 {
-	int i;
+	int num;
 
-	for (i = 1; i <= 9; i++)
-	{
-		_putchar ((i % 10) + '0');
-	}
-	_putchar ('\n');
+	for (num = 0; num <= 9; num++)
+		_putchar((num % 10) + '0');
 
+	_putchar('\n');
 }
