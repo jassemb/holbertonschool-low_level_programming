fbce6c609af21cae8c65a4cce5b6343e723d38c0
jassemb <benalijassem201@gmail.com>
2021-06-28 05:18:11 -0700

    char in rev

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -8,11 +8,11 @@ void print_rev(char *s)
 
 	int len = 0, i;
 
-	while (s[i++])
+	while (s[index++])
 		len++;
 
-	for (i = len - 1; i >= 0; i--)
-		_putchar(s[i]);
+	for (index = len - 1; index >= 0; index--)
+		_putchar(s[index]);
 
 	_putchar('\n');
 }
