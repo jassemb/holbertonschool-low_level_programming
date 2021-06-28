ef139bc90a703bf3f9e54aa5e8a6847582208b77
jassemb <benalijassem201@gmail.com>
2021-06-28 11:53:54 -0700

    puts half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+int _strlen(char *s);
+/**
+ * puts2 - prints one char out of 2 of a string, followed by a new line
+ * @str: pointer to string
+ */
+void puts_half(char *str)
+{
+	int c;
+	int len = _strlen(str);
+
+	for (c = 0; c <= (len / 2); c ++)
+		_putchar(str[c]);
+
+	_putchar('\n');
+}
+/**
+ * _strlen - returns the length of a string
+ * @s: pointer to string
+ *
+ * Return: The length of the string
+ */
+int _strlen(char *s)
+{
+	int c = 0;
+
+	while (s[c] != '\0')
+		c++;
+	return (c);
+}
