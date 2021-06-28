914cde10ae77d4c9dc23193fa9927e697f6b1ad3
jassemb <benalijassem201@gmail.com>
2021-06-28 05:14:20 -0700

    char in rev

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+ * print_rev - function that prints a string, in reverse.
+ * @s: The string to be printed.
+ */
+void print_rev(char *s)
+{
+
+	int len = 0, i;
+
+	while (*s++)
+		len++;
+
+	for (i = len - 1; i >= 0; i--)
+		_putchar(s[i]);
+
+	_putchar('\n');
+}
