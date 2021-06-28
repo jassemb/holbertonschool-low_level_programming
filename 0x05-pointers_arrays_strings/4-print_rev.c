ca030dc35e4bfdfd79cae1c4808448b67dc73a16
jassemb <benalijassem201@gmail.com>
2021-06-28 05:36:17 -0700

    char in rev

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -13,3 +13,18 @@ void print_rev(char *s)
 		_putchar(s[c]);
 	_putchar('\n');
 }
+
+/**
+ * _strlen - returns the length of a string
+ * @s: pointer to string
+ *
+ * Return: The length of the string
+ */
+int _strlen(char *s)
+{
+	int c = 0;
+
+	while (s[c] != '\0')
+		c++;
+	return (c);
+}
