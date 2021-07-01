b390b24e613c1551342dcc20dec5117fbdd3f703
jassemb <benalijassem201@gmail.com>
2021-07-01 13:01:47 -0700

    cap string

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,39 @@
+#include "holberton.h"
+
+/**
+ * cap_string - Capitalizes all words of a string.
+ * @str: The string to be capitalized.
+ *
+ * Return: A pointer to the changed string.
+ */
+char *cap_string(char *str)
+{
+	int i;
+
+	for(i=0; str[i]!='\0'; i++)
+	{
+		if(i==0)
+		{
+			if((str[i]>='a' && str[i]<='z'))
+				str[i]=str[i]-32;
+			continue;
+		}
+		if(str[i]==' ')
+		{
+			++i;
+			if(str[i]>='a' && str[i]<='z')
+			{
+				str[i]=str[i]-32;
+				continue;
+			}
+		}
+		else
+		{
+			if(str[i]>='A' && str[i]<='Z')
+			{
+				str[i]=str[i]+32;
+			}
+		}
+	}
+	return 0;
+}
