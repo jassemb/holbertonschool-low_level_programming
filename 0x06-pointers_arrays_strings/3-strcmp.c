fad21e854be58040d54ab7021d8c968468ab53e6
jassemb <benalijassem201@gmail.com>
2021-07-01 11:46:34 -0700

    The strcmp() Function in C

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -8,13 +8,10 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	while (*s1 && *s2 && *s1 == *s2)
-	{
-		s1++;
-		s2++;
-	}
-	
-	return (*s1 - *s2);
-}
-~
+	int j;
 
+	for (j = 0; s1[j] != '\0' && s2[j] != '\0'; j++)
+		if (s1[j] != s2[j])
+			return (s1[j] - s2[j]);
+	return (0);
+}
