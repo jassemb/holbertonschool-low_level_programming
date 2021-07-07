e8b56e15be2a66128bac42383a83d34a43130da5
jassemb <benalijassem201@gmail.com>
2021-07-07 03:33:00 -0700

    a function that prints a string in reverse

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -4,7 +4,7 @@
  * _print_rev_recursion - a function that prints a string in reverse.
  * @s: The string to be printed.
  */
-void _print_rev_recursion(char *s)(char *s)
+void _print_rev_recursion(char *s)
 {
 	if (*s)
 	{
