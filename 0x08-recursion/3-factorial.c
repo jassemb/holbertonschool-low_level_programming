061ec60c81f44a7f8dd90947010fb99a3feb6ae4
jassemb <benalijassem201@gmail.com>
2021-07-07 11:09:01 -0700

    factorial

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -1,4 +1,3 @@
-  
 #include "holberton.h"
 /**
  *factorial - factorial of a given number
@@ -8,6 +7,11 @@
 int factorial(int n)
 {
 	if (n == 0)
-		return 1;
-	return n * factorial(n - 1);
+		return (1);
+
+	else if (n < 0)
+		return (-1);
+
+	else
+		return (n * factorial(n - 1));
 }
