9e463df64ae0a2846c164002a0ea9b467ea2d6f4
jassemb <benalijassem201@gmail.com>
2021-07-07 10:53:06 -0700

    pow recursion

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -7,6 +7,7 @@
  */
 int _pow_recursion(int x, int y)
 {
+	int result = x;
 
 	if (y < 0)
 		return (-1);
@@ -14,5 +15,6 @@ int _pow_recursion(int x, int y)
 	else if (y == 0)
 		return (1);
 
-	return (_pow_recursion(x, y - 1));
+	result *= _pow_recursion(x, y - 1);
+	return (result);
 }
