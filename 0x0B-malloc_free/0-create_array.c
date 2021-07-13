8f5f094e60a6ecd3b25ece444d0a27452d72d4be
jassemb <benalijassem201@gmail.com>
2021-07-13 12:35:16 -0700

    Creates an array of chars and initializes it with a specific char

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,4 +1,5 @@
 #include "holberton.h"
+#include <stdlib.h>
 /**
  * create_array - Creates an array of chars and initializes it with a specific char.
  * @size: The size of the array to be initialized.
