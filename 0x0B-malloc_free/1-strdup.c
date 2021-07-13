966376920751dacac85c75aab0301e96ecb3f158
jassemb <benalijassem201@gmail.com>
2021-07-13 13:07:35 -0700

    function returns a pointer to a new string which is a duplicate of the string str

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,14 +1,15 @@
 #include "holberton.h"
 #include <stdlib.h>
-/*
+
+/**
  * _strdup - Returns a pointer to a newly-allocated space in memory
- *	containing a copy of the string given as parameter.
+ *           containing a copy of the string given as parameter.
+ * @str: The string to be copied.
  *
- *  @str: The string to be copied.
- *  Return: If str == NULL or insufficient memory is available - NULL.
- *		Otherwise - a pointer to the duplicated string.
- *
+ * Return: If str == NULL or insufficient memory is available - NULL.
+ *         Otherwise - a pointer to the duplicated string.
  */
+
 char *_strdup(char *str)
 {
 	char *sc;
