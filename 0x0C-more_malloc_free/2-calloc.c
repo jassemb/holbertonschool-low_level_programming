1c588a4564c0573d3e52d8955b2f01e982484a63
jassemb <benalijassem201@gmail.com>
2021-07-15 14:02:33 -0700

    function that allocates memory for an array, using malloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,33 +1,37 @@
 #include "holberton.h"
-#include <stdlib.h>
-
+char *_memset(char *s, char b, unsigned int n);
 /**
- * _calloc - Allocates memory for an array of a certain number
- *           of elements each of an inputted byte size.
- * @nmemb: The number of elements.
- * @size: The byte size of each array element.
+ * _calloc - allocates memory for an array, using malloc
+ * @nmemb: number of elements
+ * @size: size of elements in bytes
  *
- * Return: If nmemb = 0, size = 0, or the function fails - NULL.
- *         Otherwise - a pointer to the allocated memory.
+ * Return: pointer to allocated memory
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	void *mem;
-	char *filler;
-	unsigned int index;
+	void *memptr;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
-
-	mem = malloc(size * nmemb);
-
-	if (mem == NULL)
+	memptr = malloc(nmemb * size);
+	if (memptr == NULL)
 		return (NULL);
+	_memset(memptr, 0, (nmemb * size));
+	return (memptr);
+}
 
-	filler = mem;
+/**
+ * _memset - fills memory with a constant byte
+ * @s: character array to fill
+ * @b: constant byte to fill with
+ * @n: how many bytes to fill
+ * Return: the pointer to the char array
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
 
-	for (index = 0; index < (size * nmemb); index++)
-		filler[index] = '\0';
-
-	return (mem);
+	for (i = 0; i < n; i++)
+		s[i] = b;
+	return (s);
 }
