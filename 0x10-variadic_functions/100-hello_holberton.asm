b3bc63029450df1895e71024d4e89afe43abbd57
jassemb <benalijassem201@gmail.com>
2021-07-22 14:17:45 -0700

    asm task

---
+++ 0x10-variadic_functions/100-hello_holberton.asm
@@ -0,0 +1,18 @@
+section .data
+	msg db 10d,13d,"Hello, Holberton"
+	l equ $-msg
+
+section .text
+	global _start
+
+_start:
+	mov rax,1
+	mov rdi,1
+	mov rsi,msg
+	mov rdx,l
+	syscall
+
+	mov rax,60
+	mov rdi,0
+	syscall
+end:
