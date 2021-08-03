9aaffe0262de412a6062ad9420e20d4a48fa36f3
jassemb <benalijassem201@gmail.com>
2021-08-03 04:18:12 -0700

    get node

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -3,23 +3,6 @@
 #include <string.h>
 #include <stdio.h>
 /**
- * listint_len - returns the number of elements in a linkedlisti\
-nt_t list
-* @h: a variable that points to a listint_t struct
-* Return: number of elements
-*/
-size_t listint_len(const listint_t *h)
-{
-	int counter = 0;
-
-	while (h != NULL)
-	{
-		counter++;
-		h = h->next;
-	}
-	return (counter);
-}
-/**
  * get_nodeint_at_index - returns the nth node of listint_t list
  * @head: a variable pointing to a struct
  * @index: index of node, starting from 0
@@ -28,13 +11,17 @@ size_t listint_len(const listint_t *h)
  */
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
-	unsigned int i = 0;
-	size_t length;
+	unsigned int count = 0;
+	listint_t *current = head;
 
-	length = listint_len(head);
-	if (index > length)
-		return (NULL);
-	for (i = 0; i < index; i++)
-		head = head->next;
-	return (head);
+
+
+	while (current != NULL)
+	{
+		if (count == index)
+			return (current);
+		count++;
+		current = current->next;
+	}
+	return (NULL);
 }
