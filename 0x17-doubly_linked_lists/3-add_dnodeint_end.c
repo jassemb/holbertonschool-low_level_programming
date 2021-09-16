99ba51a81130201ccf695239a3f11728d3b89494
jassemb <benalijassem201@gmail.com>
2021-09-16 05:52:58 -0700

    add to the end of list

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -12,6 +12,7 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 	new = malloc(sizeof(dlistint_t));
 	if (new == NULL)
 		return (NULL);
+
 	new->n = n;
 	new->next = NULL;
 
@@ -21,10 +22,12 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 		*head = new;
 		return (new);
 	}
+
 	last = *head;
 	while (last->next != NULL)
 		last = last->next;
 	last->next = new;
 	new->prev = last;
+
 	return (new);
 }
