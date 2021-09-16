2ee9b0add6b9fe3595ce08c92070b20ddc01fd59
jassemb <benalijassem201@gmail.com>
2021-09-16 06:10:06 -0700

    add to the end

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -1,6 +1,6 @@
 #include "lists.h"
 /**
- * add_dnodeint - adds a new node at the end of a dlistint_t list
+ * add_dnodeint_end - adds a new node at the end of a dlistint_t list
  * @head: pointer to a pointer to a struct of type dlistint_t
  * @n: n value of new node
  * Return: address of new element, or NULL if it failed.
