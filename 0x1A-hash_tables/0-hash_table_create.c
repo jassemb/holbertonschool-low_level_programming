3a275e75dbd4be3f018a64cb64e07fe3422a2403
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 <benalijassem201@gmail.com>
2021-10-07 16:36:13 -0700

    0-hash_table_create.c

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -7,24 +7,26 @@
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	long unsigned int i;
-	
+	unsigned long int i;
 	hash_table_t *new_table = NULL;
+
 	if (size < 1)
 		return (NULL);
-	if ((new_table = calloc(1, sizeof(hash_table_t))) == NULL)
+	new_table = calloc(1, sizeof(hash_table_t));
+	if (new_table == NULL)
 	{
 		free(new_table);
 		return (NULL);
 	}
-	if ((new_table -> array = calloc(1,sizeof(sigset_t*) * size)) == NULL)
+	new_table->array = calloc(1, sizeof(sigset_t *) * size);
+	if (new_table == NULL)
 	{
 		free(new_table);
-		return NULL;
+		return (NULL);
 	}
-	for(i=0; i < size; i++)
-		new_table -> array[i] = NULL;
-	new_table -> size = size;
+	for (i = 0; i < size; i++)
+		new_table->array[i] = NULL;
+	new_table->size = size;
 	return (new_table);
 
 }
